add7c2dde51ee96c08ab430c8a1fc95bf3237470
gladiator022 <71585248+gladiator022@users.noreply.github.com>
2020-12-07 17:46:30 +0100

    remove unnecessary files

--- Frontend/Html-Css-Js/sidebar.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="styles/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="styles/welcome.css">
-    <link rel="stylesheet" href="styles/fontawesome-free-5.15.1-web/css/fontawesome.css">
-    <link rel="stylesheet" href="styles/fontawesome-free-5.15.1-web/css/solid.css">
-    <link rel="stylesheet" href="styles/fontawesome-free-5.15.1-web/css/brands.css">
-    <link rel="icon" href="images/Bloom.png" type="image/png" sizes="30x30">
-
